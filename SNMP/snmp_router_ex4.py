34d3418e4adbe34337c567eb99525066945f54d4
0dadj1an <ivohrbacek@gmail.com>
2017-01-31 15:51:40 -0800

    correction

--- SNMP/snmp_router_ex4.py
+++ SNMP/snmp_router_ex4.py
@@ -3,16 +3,19 @@
 
 import snmp_helper
    
-sys_desc = raw_input("Enter sys desc:" + '\n')
-sys_name = raw_input("Enter sys name:" + '\n')
-community_str = raw_input("Enter community string:" + '\n')
 ip_list = []
+
+sys_desc = ''
+sys_name = ''
+community_str = ''
+
+
 
 
 def checkIPFormat(ip_add):
     
     '''
-    testing function if IP address entered into cmd is correct
+    testing function if IP address entered into cmd is correct(all octetcs and only numbers)
     '''
     checker = 1
     ip4_sections = ip_add.split(".")
@@ -20,42 +23,70 @@ def checkIPFormat(ip_add):
         for item in ip4_sections:
             if item.isdigit() == False:
                 checker = 0
+    else:
+        print "Bad IP format, try it again"           
                 
     if checker == 1:
         return  True
        
     else:
-        return False 
+         print "Bad IP format, try it again"
+         return False 
 
- 
-
-def main():
+def loadData():
+    '''
+    loading user data (input) for sys_desc, sys_name, IP and snmp community string
+    '''
+    print "This is SNMP MIB2 sysName and sysDescr checke "
+    #accessing global variables
+    global sys_desc
+    global sys_name
+    global community_str
+    global ip_list
+    #load data
+    sys_desc = raw_input("Enter sys desc:" + '\n');
+    sys_name = raw_input("Enter sys name:" + '\n');
+    community_str = raw_input("Enter community string:" + '\n');
     
-    '''
-    main function for inswerting data about snmp and devices, printing sysName and sysDescr values
-    '''
-
     print "Enter IP addresses for connection and end with key word CLOSE:" + '\n'
-
-
+    # in this cyklus IP addresses for routers are loaded 
     while True:
          a = raw_input()
          if a == 'CLOSE':
-            break
+             break
          else:
               if checkIPFormat(a) == True:
-                   ip_list.append(a)
-              else:
-                   print "Bad IP format, try it again"
+                 ip_list.append(a)
+                 
+def getData(item):
+    '''
+    getting correct list for snmp_helper ->tuple with IP, string and port
+    '''
+    temp_list = (item, community_str, 161)
+    return temp_list
            
+    
+
+def main():
+    
+    
+    '''
+    main function for getting snmp data, printing sysName and sysDescr values
+    '''
+    #loads data from user
+    loadData()
+    #iterates list of IPs entered during main prompt
     for item in ip_list:
+        device_list = getData(item)# setting up correct tuple
         print "##########################"
-        for oid in (sys_desc, sys_name):
-             snmp_data = snmp_helper.snmp_get_oid(item, oid=oid)
+        for oidvalue in (sys_desc, sys_name): # iterate through OID values
+             snmp_data = snmp_helper.snmp_get_oid(device_list, oid=oidvalue)
              output = snmp_helper.snmp_extract(snmp_data) 
              print output
-             print "##############"
-                                                                                                                         
+        print "##############"
+                                                                                                                  
 
 if __name__ == "__main__":
     main()
+    
+
