31f00a6de295a05c3d100eeea85b02247a8abca7
0dadj1an <ivohrbacek@gmail.com>
2017-02-07 08:27:17 -0800

    class4 modified

--- paramiko_netmiko/class4ex.py
+++ paramiko_netmiko/class4ex.py
@@ -1,14 +1,35 @@
-import paramiko
 import time
+import paramiko
 
 
-def showVersion(self,connection):
-         connection.send("show version\n")
-         return connection.recv(5000)
 
-def enterConfig(self, connection):
-         connection.send("conf t\n")
-         return connection.recv(5000)
+def disable_paging(connection):
+    '''disable paging by entering terminal lenght'''
+    connection.send("terminal length 0\n")
+    time.sleep(1)
+    output = connection.recv(1000)
+    return output
+
+def showVersion(connection):
+    ''' method for showing the version '''
+    connection.send("show version\n")
+    time.sleep(1)
+    output = connection.recv(5000)
+    return output
+
+def enterConfig(connection):
+    '''congig mode method '''
+    connection.send("conf t\n")
+    time.sleep(1)
+    output = connection.recv(5000)
+    return output
+
+def loginBuffered(connection):
+    ''' changing login buffer '''
+    connection.send("loggin buffered 65000\n")
+    time.sleep(1)
+    output = connection.recv(5000)
+    return output
 
 
 def main():
@@ -22,16 +43,14 @@ def main():
      remote_conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
      remote_conn.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
      remote_conn02 = remote_conn.invoke_shell()
-     output = showVersion(remote_conn02)
+     
+     disable_paging(remote_conn02)
+     enterConfig(remote_conn02)
+     output = loginBuffered(remote_conn02)
+     
      print output
 
     
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
 
