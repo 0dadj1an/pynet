85f1388a98f79c526b5a95142e0c4e06e5ca01ab
0dadj1an <ivohrbacek@gmail.com>
2017-02-11 21:53:39 +0100

    modification

--- paramiko_netmiko/pexpect.py
+++ paramiko_netmiko/pexpect.py
@@ -1,8 +1,5 @@
 import pexpect
 import time
-import getpass
-
-
 
 def main():
     
@@ -13,10 +10,17 @@ def main():
     port = 8022
     remote_conn = pexpect.spawn('ssh -l {} {} -p {}'.format(username, password, port))
     remote_conn.timeout = 3
-    remote_conn.expect('ssword:')
-    remote
     
-   
+    
+    
+    remote_conn.expect('ssword:')
+    remote_conn.sendline(password)
+    
+    remote_conn.expect('#')
+    remote_conn.sendline('show ip int brief')
+    remote_conn.expect('#')
+    print remote_conn.before
+    
 
 if __name__ == "__main__":
     main()
